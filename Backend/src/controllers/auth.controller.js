4359766b958a37108e5f8ff0eea21f55dba4046d
tngu3243 <tngu3243@gmail.com>
2025-10-02 13:02:37 -0700

    Profile picture and authentication

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
@@ -83,3 +84,34 @@ export const logout = (req, res) => {
         res.stats(500).json({message: "Internal Server Error" });
     }
 };
+
+
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+
+        if(!profilePic) {
+            return res.status(400).json({message: "Profile pic is required" });
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic:uploadResponse.secure_url}, {new:true});
+
+        res.stats(200).json(updatedUser);
+
+    } catch (error){
+        console.log("error in update profile: ", error);
+        res.status(500).json({message: "Internal Server Error"});
+    }
+};
+
+
+export const checkAuth = (req,res) => {
+    try {
+        res.status(200).json(req.user);
+        } catch (error) {
+            console.log("Error in checkAuth controller", error.message);
+            res.status(500).json({ message: "Internal Server Error"});
+        }
+    }
