8047297420c6df2f5b79335f94a566122b83388c
tngu3243 <tngu3243@gmail.com>
2026-02-11 17:21:13 -0800

    Page urls

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,28 @@
+import Navbar from "./components/Navbar";
+
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+
+import { Routes, Route } from "react-router-dom";
+
 const App = () => {
-  return <div className="text-red-500">Hello</div>;
+  return (
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+
+      </Routes>
+    </div>
+  );
 };
+
 export default App;
