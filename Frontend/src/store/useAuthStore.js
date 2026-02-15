165ecd55325355b921930d416272c06e23a5209b
tngu3243 <tngu3243@gmail.com>
2026-02-14 18:34:53 -0800

    Navbar

--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -36,4 +36,14 @@ export const useAuthStore = create((set) => ({
         }
     },
 
+    logout: async () => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({ authUser: null });
+            toast.success("Logged out successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+    },
+
 }));
