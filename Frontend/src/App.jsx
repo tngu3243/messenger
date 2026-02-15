7a9b6d6e2c91b1b18519c8c264d7615ff27c4109
tngu3243 <tngu3243@gmail.com>
2026-02-15 01:45:02 -0800

    Settings/Color page updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,6 +8,7 @@ import ProfilePage from "./pages/ProfilePage";
 
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
 import { useEffect } from "react";
 
 import { Loader } from "lucide-react";
@@ -15,6 +16,7 @@ import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const { authUser,checkAuth, isCheckingAuth } = useAuthStore();
+  const { theme } = useThemeStore();
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
@@ -29,7 +31,7 @@ const App = () => {
   );
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
 
       <Routes>
